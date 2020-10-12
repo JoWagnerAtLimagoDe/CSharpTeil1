08c3f23c4e14baa4e51bcd28206c90f7d43aab3b
limagoHub <limagoHub@limago.de>
2020-10-12 15:23:53 +0200

    letzter Stand

--- Tag1_02StapelMitNUnit/Tag1_02StapelMitNUnitTests/StapelTests.cs
+++ Tag1_02StapelMitNUnit/Tag1_02StapelMitNUnitTests/StapelTests.cs
@@ -3,13 +3,14 @@ using Bundesbank.Collections;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Tag1_02StapelMitNUnit;
 
 namespace Bundesbank.Collections.Tests
 {
     [TestFixture()]
     public class StapelTests
     {
-      public Stapel objectUnderTest { get; set; }
+      private Stapel objectUnderTest { get; set; }
 
        [SetUp]
        public void SetUp()
@@ -43,5 +44,74 @@ namespace Bundesbank.Collections.Tests
             Assert.IsFalse(ergebnis);
         }
 
+        [Test]
+        public void Push_HappyDay_Success()
+        {
+            // Arrange
+            object v = new Object();
+
+
+            // Action
+            objectUnderTest.Push(v);
+
+            //  Assertion
+            Assert.AreSame(v, objectUnderTest.Pop());
+
+        }
+
+        [Test]
+        public void Push_FillUpToLimit_noExceptionIsThrown()
+        {
+            // Arrange
+
+
+            // Action
+            fillUpToLimit();
+
+            //  Assertion
+            // No Excpetion is thrown
+
+        }
+
+        [Test]
+        public void Push_Overflow_throwsStapelException()
+        {
+            // Arrange
+            fillUpToLimit();
+
+
+            // Action
+            StapelException se = Assert.Throws<StapelException> (() => objectUnderTest.Push(new object()));
+            Assert.AreEqual("Overflow", se.Message);
+
+
+        }
+        [Test]
+        public void Push_Overflow_throwsStapelException_Alternative()
+        {
+            try
+            {
+                // Arrange
+                fillUpToLimit();
+
+
+                // Action
+                objectUnderTest.Push(new object());
+                Assert.Fail("Upps");
+            }
+            catch (StapelException e)
+            {
+                Assert.AreEqual("Overflow", e.Message);
+                
+            }
+        
+
+        }
+
+        private void fillUpToLimit()
+        {
+            for (int i = 0; i < 10; i++)
+                objectUnderTest.Push(new object());
+        }
     }
 }
