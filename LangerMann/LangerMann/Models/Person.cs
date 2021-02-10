8d2aa2dfc60ece7cec1ed6acf43adc4abcfb786a
limagoHub <limagoHub@limago.de>
2021-02-10 09:00:50 +0100

    logger

--- LangerMann/LangerMann/Models/Person.cs
+++ LangerMann/LangerMann/Models/Person.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LangerMann.Models
 {
     public class Person
     {
-        public Person()
-        {
+  
+       [Key]
+       [Required]
+       public Guid PersonId { get; set; }
 
-        }
-
+        
+        [StringLength(30)]
+        [Required]
+        public string Vorname { get; set; }
+        
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
+        public string Nachname { get; set; }
+        
         public override string ToString() => $"{nameof(PersonId)}: {PersonId}, {nameof(Vorname)}: {Vorname}, {nameof(Nachname)}: {Nachname}";
 
-
-        [Key]
-        public Guid PersonId { get; set; }
-        public string Vorname { get; set; }
-        public string Nachname { get; set; }
-
-      
     }
 }
